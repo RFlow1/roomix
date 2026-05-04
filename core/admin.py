449091683e51f5e1fde9dd64c7b08a249e02edd3
Austin <austinflowers2019@gmail.com>
2026-05-03 19:57:32 -0500

    Fix models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, Listing, ListingImage
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Listing)
+admin.site.register(ListingImage)
